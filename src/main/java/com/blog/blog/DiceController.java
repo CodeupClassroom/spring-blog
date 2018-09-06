3968c5c9e54d57ae4f33a67b497f409be6bf9817
Justin Reich <justinreich.dev@gmail.com>
2018-09-06 13:41:00 -0500

    add dice exercise solution and partials for posts

---
+++ src/main/java/com/blog/blog/DiceController.java
@@ -0,0 +1,31 @@
+package com.blog.blog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Random;
+
+@Controller
+public class DiceController {
+
+    @GetMapping("/roll-dice")
+    public String returnRollDice() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{num}")
+    public String returnResults(@PathVariable int num, Model model) {
+        model.addAttribute("answer", generateRandomNum());
+        model.addAttribute("userGuess", num);
+        return "roll-results";
+    }
+
+    private int generateRandomNum() {
+        Random random = new Random();
+        return random.nextInt((6 - 1) + 1) + 1;
+    }
+
+
+}
